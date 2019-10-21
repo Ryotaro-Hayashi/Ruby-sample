7e4d7fadadc3e81abb340e0ebd86714531c1e1d8
hysrtr <hys.rtr.forit>
2019-10-21 20:37:51 +0900

    [add] コメントを追加

--- test/controllers/hello_controller_test.rb
+++ test/controllers/hello_controller_test.rb
@@ -7,7 +7,6 @@ class HelloControllerTest < ActionDispatch::IntegrationTest
   test "list action" do
     # コントローラーを起動するためにgetメソッドで疑似的にHTTPリクエストを生成
     get hello_path
-    assert_equel 10, assigns(:books).length, 'found rows is wrong.'
     # 指定されたHTTPステータスが返されたらテスト成功
     assert_response :success, 'list action failed.'
     assert_template hello_path
