513f37f2cf423f490dfdbb738d733bd2506e68d2
hysrtr <hys.rtr.forit>
2019-10-22 18:39:30 +0900

    [add] 画像が自動で表示されるように、ヘルパーを作成・使用

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user)
+    # GravatarのURLはユーザーのメールアドレスをMD5という仕組みでハッシュ化
+    # Digestライブラリのhexdigestメソッドを使うと、MD5のハッシュ化が実現
+    # MD5ハッシュでは大文字と小文字が区別されるので、Rubyのdowncaseメソッドを使ってhexdigestの引数を小文字に変換
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # ユーザーのメールアドレスを組み込んだGravatar専用の画像パス
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
