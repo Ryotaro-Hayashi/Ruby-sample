78149ed5af890e828d8c72044a1ffed429333b84
hysrtr <hys.rtr.forit>
2019-10-22 18:19:42 +0900

    [try] デバッガーメソッドを使ってみた。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  # RailsのREST機能が有効になっていると、GETリクエストは自動的にshowアクションとして扱わ
   def show
     # params[:id]の部分は、たとえばユーザーidの1に置き換わる。
     @user = User.find(params[:id])
+    # デバッガーメソッドを追加すると、デバッグが簡単に行える。
+    # debugger
   end
 
   def new
