7347304f35f508386c9a3c519b3815f961f6cab4
hysrtr <hys.rtr.forit>
2019-10-23 21:33:38 +0900

    [add] createアクションで、paramsを使ってフォーム送信を受け取り、User.newを使って新しいユーザーオブジェクトを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  # RESTful URI に応答するので、/usersへのPOSTリクエストはcreateアクションに送られる。
+  def create
+    # paramsでフォーム送信を受け取り、User.newを使って新しいユーザーオブジェクトを作成
+    @user = User.new(params[:user]) # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
 end
