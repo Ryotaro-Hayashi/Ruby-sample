5fa9fa10f7933638a76a8fbe91cb58eb3c336b2c
hysrtr <hys.rtr.forit>
2019-10-24 01:43:57 +0900

    [add] 無効なユーザー登録に対するテストを投稿

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid signup information" do
+    # getメソッドを使ってユーザー登録ページにアクセス
+    get signup_path
+    # assert_no_differenceのブロックを実行する前後でUser.countの値が変わらないことをテスト
+    assert_no_difference 'User.count' do
+      # フォーム送信をテストするために、users_pathに対してPOSTリクエストを送信
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    # 送信に失敗したときにnewアクションが再描画されるので、assert_templateを使ったテスト
+    # assert_template は指定されたテンプレートファイルが選択されたかをテスト
+    assert_template 'users/new'
+  end
 end
