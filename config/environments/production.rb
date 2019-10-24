0c45b7bb115f984e501d642808b22542d62d34b6
hysrtr <hys.rtr.forit>
2019-10-24 12:57:41 +0900

    [add] SSLを使うように強制

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 本番環境ではSSLを使うように矯正
+  config.force_ssl = true
 end
