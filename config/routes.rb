d08c53efa7946e613d16d409d1631b180f6b088c
hysrtr <hys.rtr.forit>
2019-11-10 00:46:39 +0900

    [command] Sessionsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # railsでは、クライアントからの要求を受け取ると、ルーティングを利用して呼び出すべきコントローラー/アクションを決定する。
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # ～/helloというURLが要求されたら、helloコントローラーのindexアクションを呼び出す。
   get '/hello',  to: 'hello#greet'
   get '/list',   to: 'hello#list'
