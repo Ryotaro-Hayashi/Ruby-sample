a1bd57479d750a097a002223073941f22a59378d
hysrtr <hys.rtr.forit>
2019-11-10 01:41:20 +0900

    [add] セッションを使ってデータベースからユーザーを探す

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # paramsは次のような入れ子ハッシュになっている。
+    user = User.find_by(email: params[:session][:email].downcase)
+    # authenticateメソッドで、引数に渡された文字列をハッシュ化した値と、データベース内にあるpassword_digestカラムの値を比較
+    # 入力されたメールアドレスを持つユーザーがデータベースに存在し、かつ入力されたパスワードがそのユーザーのパスワードである場合
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+  end
+
+  def destroy
+  end
 end
