c2d1c2420db876a739221ab4aa3d0bd5c78a8e75
hysrtr <hys.rtr.forit>
2019-11-13 22:57:51 +0900

    [add] fixture を利用

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    # マイクロポストの作成
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  
 end
