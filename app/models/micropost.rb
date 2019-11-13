dbac86af994fddc5801b077f1b8d094a06213011
hysrtr <hys.rtr.forit>
2019-11-13 21:29:10 +0900

    [add] index に複数のカラムを設定（index を張る）

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
+  # コマンドでマイクロポストモデルを作るのオプションで
+  # user:references と指定したので自動生成で、
+  # micropostモデルをユーザーモデルと結び付けてくれる。
+  # belongs_to は１対１の関係であることを表す。
   belongs_to :user
 end
--- db/migrate/20191113115427_create_microposts.rb
+++ db/migrate/20191113115427_create_microposts.rb
@@ -2,9 +2,14 @@ class CreateMicroposts < ActiveRecord::Migration[5.1]
   def change
     create_table :microposts do |t|
       t.text :content
+      # コマンドで、user:references と指定したので自動生成で、
+      # 自動的にインデックスと外部キー参照付きのuser_idカラムが追加される。
       t.references :user, foreign_key: true
 
       t.timestamps
     end
+    # indexに複数のカラムを設定する場合は、add_index テーブル名, [:カラム名1, :カラム名2, ・・・]
+    # index とは、特定のカラムからデータを取得する際に、テーブルの中の特定のカラムのデータを複製し検索が行いやすいようにしたもの
+    add_index :microposts, [:user_id, :created_at]
   end
 end
