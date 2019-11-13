f5b78a7cb952ebc35cce274e762258a98e0fe102
hysrtr <hys.rtr.forit>
2019-11-13 23:01:51 +0900

    [add] user_id によるマイクロポストのテスト

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
+  # fixtures を利用
   def setup
     @user = users(:michael)
     # このコードは慣習的に正しくない
@@ -8,5 +9,14 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
   end
 
-  
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  # ユーザーidがないときのマイクロポストのテスト
+  test 'user id should be present' do
+    # ユーザーidを空にする
+    @user.valid.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
