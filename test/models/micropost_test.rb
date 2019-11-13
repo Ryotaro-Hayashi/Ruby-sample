4d0366311ad5458f68e82556ca3a2ea65dbd0b8e
hysrtr <hys.rtr.forit>
2019-11-13 23:47:17 +0900

    [add] テスト駆動開発で、マイクロポストの内容がないときと、文字数が140字以上のときのテスト

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,6 @@ class MicropostTest < ActiveSupport::TestCase
   # fixtures を利用
   def setup
     @user = users(:michael)
-    # このコードは慣習的に正しくない
     # マイクロポストの作成
     @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
   end
@@ -17,6 +16,18 @@ class MicropostTest < ActiveSupport::TestCase
   test 'user id should be present' do
     # ユーザーidを空にする
     @user.valid.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  # マイクロポストがスペースであるときのテスト
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  # 140文字以上のマイクロポストのテスト
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
 end
