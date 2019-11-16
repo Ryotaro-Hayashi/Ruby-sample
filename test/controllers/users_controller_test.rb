3cadd39ef1a04d61bbcd71e9e9d72a5bccf5158e
hysrtr <hys.rtr.forit>
2019-11-16 18:00:24 +0900

    [add] beforeフィルターが機能していることのテスト

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  # ログインしていないときに編集しようとするときのテスト
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    # beforeフィルターによって実行されるlogged_in_userメソッドでフラッシュメッセージを表示
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  # ログインしていないときに直接ユーザー情報を更新しようとするときのテスト
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
 end
