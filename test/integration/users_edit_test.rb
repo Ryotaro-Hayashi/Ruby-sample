22e4ecc4eadf581058d08746a772bfd476175f4e
hysrtr <hys.rtr.forit>
2019-11-16 17:38:29 +0900

    [add] テストでは、ログインしてからユーザー情報を更新できるようにする。

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     # edit_user_path にgetリクエストを送って、編集ページへ
     get edit_user_path(@user)
     assert_template 'users/edit'
@@ -18,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'successful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
