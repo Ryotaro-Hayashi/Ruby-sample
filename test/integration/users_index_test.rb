a7fedca3bbbcb9bb0b6c9e84adedcc9f43af21b4
hysrtr <hys.rtr.forit>
2019-11-18 15:16:33 +0900

    [add] ページネーション機能のテストを追加

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  # indexページでユーザーが表示されていて、ページネーション機能が実装されていることのテスト
+  test "index including pagination" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    # divタグにpaginationクラスがあるかをテスト（ページネーションのリンクがある。）
+    assert_select 'div.pagination'
+    # ページにユーザーが存在する。
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
 end
