52ba494da5e95155867438325d9d03a2feb6682d
hysrtr <hys.rtr.forit>
2019-11-18 12:46:12 +0900

    [command] ページネーション機能の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # サンプルユーザーの作成
-
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar")
 
+# サンプルユーザーを99個作成
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
