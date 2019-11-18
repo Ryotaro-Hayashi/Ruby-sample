66a5d40db5df69dc8a35f70c93150d901a68e088
hysrtr <hys.rtr.forit>
2019-11-18 16:03:58 +0900

    [add] 管理者だけにユーザー削除のリンクを表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 # サンプルユーザーを99個作成
 99.times do |n|
