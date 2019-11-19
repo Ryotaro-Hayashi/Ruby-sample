dc71cb374346219183541eaf809d99d70b62da80
hysrtr <hys.rtr.forit>
2019-11-19 12:14:32 +0900

    [add] サンプルユーザーの最初の6人にマイクロポストを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,12 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
+
+# 最初の6人を抜き出す。
+users = User.order(:created_at).take(6)
+50.times do
+  # 投稿内容は、Faker::Loremメソッドで作成
+  content = Faker::Lorem.sentence(5)
+  # 各ユーザーにマイクロポストを作成
+  users.each { |user| user.microposts.create!(content: content) }
+end
