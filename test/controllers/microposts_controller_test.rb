36c0a59f28f1c84aab81ddd87c038fd52c8d2b55
hysrtr <hys.rtr.forit>
2019-11-19 14:25:06 +0900

    [add] ログインしていないときにマイクロポストを作成・削除しようとするときのテスト

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  # ログインしていないときにマイクロポストを作成しようとするときのテスト
+  test "should redirect create when not logged in" do
+    # POSTリクエストでマイクロポストを作成しようとしても、マイクロポストの数が変わらない
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  # ログインしていないときにマイクロポストを削除しようとするときのテスト
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
